5a3b860886a0ee84d2767b6fbbc6d6c4f8482b8a
angga <anggashela@gmail.com>
2018-08-08 06:38:43 -0400

    absensi pertgl 8agustus

---
+++ src/java/controller/java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.Grid;
+
+/**
+ *
+ * @author angga
+ */
+public class java extends GenericForwardComposer {
+    public void onClick$fnList(){
+        alert("asdas");
+    }
+    
+    
+}
